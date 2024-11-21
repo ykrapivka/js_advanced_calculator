a879b7a0222a9537f06cc57aab1ad61cdfc59ec2
Yehor <y.krapivka18@gmail.com>
2024-11-21 11:16:59 -0500

    final

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -1,10 +1,34 @@
 'use strict';
 
-/**
- * @return {object}
- */
 function makeCalculator() {
-  // write code here
+  const calculator = {
+    result: 0,
+
+    operate: (callback, number) => {
+      callback(number);
+
+      return calculator;
+    },
+    add: (number) => {
+      calculator.result += number;
+    },
+    subtract: (number) => {
+      calculator.result -= number;
+    },
+    multiply: (number) => {
+      calculator.result *= number;
+    },
+    divide: (number) => {
+      calculator.result /= number;
+    },
+    reset: () => {
+      calculator.result = 0;
+
+      return calculator;
+    },
+  };
+
+  return calculator;
 }
 
 module.exports = makeCalculator;
